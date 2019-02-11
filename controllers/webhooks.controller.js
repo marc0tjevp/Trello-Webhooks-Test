a3f2e220917578a42f367f76441db603c8784a08
Marco van Poortvliet <Marc0tjevp@gmail.com>
2019-02-11 17:32:19 +0100

    log body

--- controllers/webhooks.controller.js
+++ controllers/webhooks.controller.js
@@ -1,5 +1,6 @@
 let post = (req, res) => {
-    console.log("Name: " + req.body.model.name + " | Action: " + req.body.action.type + " | By: " + req.body.action.memberCreator.fullName)
+    console.log(req.body)
+    // console.log("Name: " + req.body.model.name + " | Action: " + req.body.action.type + " | By: " + req.body.action.memberCreator.fullName)
     res.status(200).json({
         "message": "example route"
     }).end()
